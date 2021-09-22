28b2556c1ec0d95c66598e5febad6feacb17584c
MaulidioPoncoNugroho <maulidioponco@gmail.com>
2021-09-22 21:23:47 +0700

    tugas2

---
+++ application/controllers/tugas2.php
@@ -0,0 +1,14 @@
+<?php
+class tugas2 extends CI_Controller
+{
+    public function index()
+    {
+        echo "</h1> Perkenalkan <br> </h1>";
+        echo "Nama : Maulidio Ponco Nugroho <br>
+        Tempat Lahir : Bogor <br>
+        Tanggal Lahir : 20 Agustus 1994 <br>
+        Tempat tinggal : Perum Villa Permata Mas Blok BN 21,Bojongnangka <br>
+        Motivasi : Untuk mencari pengalaman dan belajar tentang sistem informasi beserta mata kuliah yang lain
+         ";
+    }
+}
