6b062bc6e544e3de29d42900cf928b6d540290e5
MaulidioPoncoNugroho <maulidioponco@gmail.com>
2021-09-22 19:30:34 +0700

    main

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1> mybio </h1>";
+        echo "nama : Maulidio <br>
+            tempat lahir : bogor <br>
+            tanggal lahir : 20 agustus 1994 <br>";
+    }
+}
