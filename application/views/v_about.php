a9cbbadf4c489e2903dd19032d5ce9b160d0a94b
maulidioPoncoNugroho <maulidioponco@gmail.com>
2021-10-07 20:41:32 +0700

    commit kedua

---
+++ application/views/v_about.php
@@ -0,0 +1,10 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <p>
+        Halaman About <br>
+        Nama : Maulidio Ponco Nugroho <br>
+        NIM : 12192069 <br>
+        Kelas : 12.3D.04
+    </p>
+
+</section>
---
+++ application/views/v_index.php
@@ -0,0 +1,9 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus vitae consectetur asperiores qui laborum reiciendis iste recusandae, amet rerum nemo, obcaecati iure corporis fuga numquam reprehenderit nulla assumenda inventore pariatur.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus vitae consectetur asperiores qui laborum reiciendis iste recusandae, amet rerum nemo, obcaecati iure corporis fuga numquam reprehenderit nulla assumenda inventore pariatur.
+    </p>
+</section>
